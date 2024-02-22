f72d643747fdb2f9b37bf53dbc5714b1acb196c8
Artem Khymenko <artem.khymenko@gmail.com>
2024-02-22 17:52:39 +0200

    implement crud

---
+++ src/backend/app/crud.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+
+from app import models, schemas
+
+def create_item(db: Session, item: schemas.ItemCreate):
+    db_item = models.Item(**item.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+def get_items(db: Session, item_id: int):
+    return db.query(models.Item).filter(models.Item.id == item_id).first()
+
+def get_items(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
+def update_item(db: Session, item_id: int, item: schemas.Item):
+    db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    db_item.title = item.title
+    db_item.description = item.description
+    db_item.price = item.price
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+def delete_item(db: Session, item_id: int):
+    db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    db.delete(db_item)
+    db.commit()
+    return db_item
