67fbde0153457a13fad51b91be043a6b529fabda
Artem Khymenko <artem.khymenko@gmail.com>
2024-02-22 17:46:12 +0200

    create models

---
+++ src/backend/app/models.py
@@ -0,0 +1,15 @@
+# Represent database models
+
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    price = Column(Float)
+
