4e35c70a83fb415bae8a78807f839b26ba2dedda
Artem Khymenko <artem.khymenko@gmail.com>
2024-03-14 09:56:57 +0200

    add pydantic schemas for place, country, supermarkets

--- src/backend/app/schemas.py
+++ src/backend/app/schemas.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
@@ -14,3 +15,46 @@ class Item(ItemBase):
 
     class Config:
         orm_mode = True
+
+class PlaceBase(BaseModel):
+    name: str
+
+class PlaceCreate(PlaceBase):
+    country_id: int
+    supermarket_id: int
+
+class Place(PlaceBase):
+    id: int
+    
+    country_id: int
+    supermarket_id: int
+
+    class Config:
+        orm_mode = True
+
+class CountryBase(BaseModel):
+    name: str
+
+class CountryCreate(CountryBase):
+    pass
+
+class Country(CountryBase):
+    id: int
+
+    places: Optional[list[Place]] = []
+    class Config:
+        orm_model = True
+
+class SupermarketBase(BaseModel):
+    name: str
+
+class SupermarketCreate(SupermarketBase):
+    pass
+
+class Supermarket(SupermarketBase):
+    id: int
+    places: Optional[list[Place]] = []
+
+    class Config:
+        orm_model = True
+
