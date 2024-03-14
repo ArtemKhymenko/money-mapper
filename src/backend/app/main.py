4a1cf55133bc2551b66870b52192da7c717d6b6c
Artem Khymenko <artem.khymenko@gmail.com>
2024-03-14 09:59:45 +0200

    edit main.py

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,24 +1,8 @@
-import uvicorn
+# Create API Endpoints
 from fastapi import FastAPI
+
+from routes import router
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+app.include_router(router)
