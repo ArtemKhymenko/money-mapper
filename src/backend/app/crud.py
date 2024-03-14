0ad6d1da56f2bbd46384a9d30c542b6d05fb22ed
Artem Khymenko <artem.khymenko@gmail.com>
2024-03-14 09:53:44 +0200

    create simple crud operations

--- src/backend/app/crud.py
+++ src/backend/app/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from app import models, schemas
+import models, schemas
 
 def create_item(db: Session, item: schemas.ItemCreate):
     db_item = models.Item(**item.dict())
@@ -9,7 +9,7 @@ def create_item(db: Session, item: schemas.ItemCreate):
     db.refresh(db_item)
     return db_item
 
-def get_items(db: Session, item_id: int):
+def get_item(db: Session, item_id: int):
     return db.query(models.Item).filter(models.Item.id == item_id).first()
 
 def get_items(db: Session, skip: int = 0, limit: int = 10):
@@ -29,3 +29,57 @@ def delete_item(db: Session, item_id: int):
     db.delete(db_item)
     db.commit()
     return db_item
+
+def create_country(db: Session, country: schemas.CountryCreate):
+    db_country = models.Country(**country.dict())
+    db.add(db_country)
+    db.commit()
+    db.refresh(db_country)
+    return db_country
+
+def get_country(db: Session, country_id: int):
+    return db.query(models.Country).filter(models.Country.id == country_id).first()
+
+def get_countries(db: Session):
+    return db.query(models.Country).all()
+
+def update_country(db: Session, country_id: int, country: schemas.CountryCreate):
+    db_country = db.query(models.Country).filter(models.Country.id == country_id).first()
+    for key, value in country.dict().items():
+        setattr(db_country, key, value)
+    db.commit()
+    db.refresh(db_country)
+    return db_country
+
+def delete_country(db: Session, country_id: int):
+    db_country = db.query(models.Country).filter(models.Country.id == country_id).first()
+    db.delete(db_country)
+    db.commit()
+    return db_country
+
+def create_supermarket(db: Session, supermarket: schemas.SupermarketCreate):
+    db_supermarket = models.Supermarket(**supermarket.dict())
+    db.add(db_supermarket)
+    db.commit()
+    db.refresh(db_supermarket)
+    return db_supermarket
+
+def get_supermarket(db: Session, supermarket_id: int):
+    return db.query(models.Supermarket).filter(models.Supermarket.id == supermarket_id).first()
+
+def get_supermarkets(db: Session):
+    return db.query(models.Supermarket).all()
+
+def update_supermarket(db: Session, supermarket_id: int, supermarket: schemas.SupermarketCreate):
+    db_supermarket = db.query(models.Supermarket).filter(models.Supermarket.id == supermarket_id).first()
+    for key, value in supermarket.dict().items():
+        setattr(db_supermarket, key, value)
+    db.commit()
+    db.refresh(db_supermarket)
+    return db_supermarket
+
+def delete_supermarket(db: Session, supermarket_id: int):
+    db_supermarket = db.query(models.Supermarlet).filter(models.Supermarket.id == supermarket_id).first()
+    db.delete(db_supermarket)
+    db.commit()
+    return db_supermarket
